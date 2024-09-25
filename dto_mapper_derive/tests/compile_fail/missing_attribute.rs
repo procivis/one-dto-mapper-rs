319e9fc6d6976d3190cc53b103be7b715f2bc575
Developer <info@procivis.ch>
2024-09-25 10:04:54 +0200

    ONE-3488: rename dto-mapper to one-dto-mapper

--- dto_mapper_derive/tests/compile_fail/missing_attribute.rs
+++
@@ -1,6 +0,0 @@
-use dto_mapper_derive::From;
-
-#[derive(From)]
-struct X {}
-
-fn main() {}
---
+++ one-dto-mapper/examples/try_into_and_try_from/replace.rs
@@ -0,0 +1,50 @@
+use one_dto_mapper::{TryFrom, TryInto};
+
+struct UserName(String);
+
+impl TryFrom<String> for UserName {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Ok(UserName(value))
+    }
+}
+
+impl From<UserName> for String {
+    fn from(value: UserName) -> Self {
+        value.0
+    }
+}
+
+struct PersonDto {
+    name: String,
+}
+
+#[derive(TryFrom)]
+#[try_from(T = PersonDto, Error = String)]
+struct FromPerson {
+    name: UserName,
+
+    #[try_from(replace = "0u16")]
+    age: u16,
+}
+
+#[derive(TryFrom, TryInto)]
+#[try_from(T = PersonDto, Error = String)]
+#[try_into(T = PersonDto, Error = String)]
+struct IntoFromPerson {
+    #[try_into(infallible)]
+    name: UserName,
+
+    #[try_into(skip)]
+    #[try_from(replace = "0u16")]
+    age: u16,
+}
+
+fn main() {
+    let p = PersonDto {
+        name: "Joe".to_string(),
+    };
+
+    let p2 = IntoFromPerson::try_from(p).unwrap();
+}
